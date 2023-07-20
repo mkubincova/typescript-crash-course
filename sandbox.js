4e7c9add0e866b205c9b7fda058c4104395752e4
mkubincova <mkubincova@proton.me>
2023-07-20 10:25:35 +0200

    feat: inferred types

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,19 @@
+// ***** INFERRED TYPES ***** //
+// variables
 var character = 'mario';
-console.log(character);
+var age = 30;
+var isBlackBelt = false;
+// parameters
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+// arrays
+var fruits = ['apple', 'peach', 'banana'];
+var numbers = [10, 20, 30, 40];
+var mixed = ['apple', 10, false];
+// objects
+var ninja = {
+    name: 'mario',
+    enrolled: true,
+    age: 30
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,23 @@
-const character = 'mario';
+// ***** INFERRED TYPES ***** //
 
-console.log(character);
+// variables
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
+
+// parameters
+const circle = (diameter: number) => {
+    return diameter * Math.PI;
+};
+
+// arrays
+let fruits = ['apple', 'peach', 'banana'];
+let numbers = [10, 20, 30, 40];
+let mixed = ['apple', 10, false];
+
+// objects
+let ninja = {
+    name: 'mario',
+    enrolled: true,
+    age: 30
+};
