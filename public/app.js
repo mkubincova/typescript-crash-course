f099e67b400e9452739a41667ed7c0994179629b
mkubincova <mkubincova@proton.me>
2023-07-20 15:10:09 +0200

    feat: generics

--- public/app.js
+++ public/app.js
@@ -23,3 +23,19 @@ form.addEventListener('submit', (e) => {
     }
     list.render(doc, type.value, 'end');
 });
+// generics
+const addUID = (obj) => {
+    let uid = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { uid });
+};
+let docOne = addUID({ name: 'yoshi', age: 40 });
+const docThree = {
+    uid: 1,
+    resourceName: 'person',
+    data: { text: 'dafsdf' }
+};
+const docFour = {
+    uid: 2,
+    resourceName: 'person',
+    data: ['dafsdf']
+};
--- src/app.ts
+++ src/app.ts
@@ -30,3 +30,30 @@ form.addEventListener('submit', (e: Event) => {
 
     list.render(doc, type.value, 'end');
 });
+
+
+// generics
+const addUID = <T extends { name: string; }>(obj: T) => {
+    let uid = Math.floor(Math.random() * 100);
+    return { ...obj, uid };
+};
+
+let docOne = addUID({ name: 'yoshi', age: 40 });
+
+interface Resource<T> {
+    uid: number,
+    resourceName: string,
+    data: T;
+}
+
+const docThree: Resource<object> = {
+    uid: 1,
+    resourceName: 'person',
+    data: { text: 'dafsdf' }
+};
+
+const docFour: Resource<string[]> = {
+    uid: 2,
+    resourceName: 'person',
+    data: ['dafsdf']
+};
